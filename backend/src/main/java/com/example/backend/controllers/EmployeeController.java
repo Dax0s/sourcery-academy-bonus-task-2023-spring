70150940e7afc01fbcca0913350a0ef3339ab53a
Domas K <klimaviciusdomas@gmail.com>
2023-02-08 19:36:07 +0200

    Remove unused import statements

--- backend/src/main/java/com/example/backend/controllers/EmployeeController.java
+++ backend/src/main/java/com/example/backend/controllers/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.example.backend.controllers;
 
-import com.example.backend.dtos.EmployeeCreateDto;
 import com.example.backend.models.Employee;
 import com.example.backend.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/employee")
